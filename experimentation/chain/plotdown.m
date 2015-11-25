db06efa0e1d96c84f6b653f912c9bc148f73e464
Steffan Norberhuis <steffan@norberhuis.nl>
2015-11-25 14:48:26 +0100

    Added simple chain experiment

---
+++ experimentation/chain/plotdown.m
@@ -0,0 +1,13 @@
+% Import midsdatadowndown.dat
+x = 1:10;
+
+figure('position', [0,0,800,500])
+
+hold on;
+plot(x, midsdatadown(2,:), ':*')
+plot(x, midsdatadown(1,:), ':o')
+
+
+xlabel('Sequence number of the block')
+ylabel('Total download amount in MB')
+legend('Seeder', 'Downloader','Location', 'southeast')
