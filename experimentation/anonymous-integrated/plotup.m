71093d9610e97caefaa1bf9abf27ee510d19f14e
Steffan Norberhuis <steffan@norberhuis.nl>
2015-12-02 14:21:59 +0100

    Anonymous integrated experiment: added more observations

--- experimentation/anonymous-integrated/plotup.m
+++ experimentation/anonymous-integrated/plotup.m
@@ -3,10 +3,11 @@ x = 1:145;
 figure('position', [0,0,800,500])
 
 hold on;
+plot(x, midsdataup(2,:), ':>')
+plot(x, midsdataup(3,:), ':h')
+plot(x, midsdataup(4,:), ':o')
 plot(x, midsdataup(1,:), ':+')
-plot(x, midsdataup(2,:), ':+')
-plot(x, midsdataup(3,:), ':+')
-plot(x, midsdataup(4,:), ':+')
+
 xlabel('Sequence number of the block')
 ylabel('Total upload amount in MB')
-%legend('seeder','hop 1','hop 2','leecher','Location','southeast')
+legend('seeder','hop 1','hop 2','leecher','Location','southeast')
